af64f19699830d7850d744cff42eb08ace6d022a
akashmengji <155937604+akashmengji@users.noreply.github.com>
2025-07-18 19:13:54 +0530

    RTL for Binary to gray

--- rtl/binary_to_bcd.sv
+++ rtl/binary_to_bcd.sv
@@ -2,17 +2,19 @@
 module binary_to_gray #(
     parameter WIDTH = 6  // Define the bit width of the input
 ) (
-    input  wire [WIDTH-1:0] binary_in,  
-    output reg  [WIDTH-1:0] gray_out   
+    input  wire [WIDTH-1:0] binary_in,
+    output wire [WIDTH-1:0] gray_out
 );
 
-  integer i;
-  always @(*) begin
-    gray_out[WIDTH-1] = binary_in[WIDTH-1];  
+  // Assign MSB directly
+  assign gray_out[WIDTH-1] = binary_in[WIDTH-1];
 
-    for (i = WIDTH - 2; i >= 0; i = i - 1) begin
-      gray_out[i] = binary_in[i+1] ^ binary_in[i];
+  // Generate loop for the remaining bits
+  genvar i;
+  generate
+    for (i = 0; i < WIDTH - 1; i = i + 1) begin
+      assign gray_out[i] = binary_in[i+1] ^ binary_in[i];
     end
-  end
+  endgenerate
 
 endmodule
